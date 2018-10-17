2000f44c47bab45e71c0bc67e5084dfd56f4a357
Eva Grench <grenche@carleton.edu>
2018-10-17 10:54:52 -0500

    Added some changes

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 print()
 print('Quesiton 1')
 
+w = 0
 x = 100
 y = 10
 z = 55
@@ -11,6 +12,7 @@ for i in range(4):
 
 y = -50
 print(y % z)
+print(w)
 
 
 
@@ -28,6 +30,7 @@ print('Quesiton 3')
 for i in range(20):
     if i % 4 == 1:
         print("i", i)
+        print(type(i))
         
 
 print()
@@ -42,17 +45,19 @@ elif age > 50:
     print(fave_fruit)
 if fave_fruit != 'apple':
     print(fave_color)
+else:
+    print('conflict?')
 
     
 print()
-print('Quesiton 5')  
+print('Quesiton Five')  
     
 a = 3
 while a > 0:
     for i in range(2):
         print(a)
     a = a - 1
-print("a")
+print(a)
 
 
 
@@ -64,12 +69,13 @@ num_a = 0
 num_b = 0
 for i in range(1, my_range + 1):
     if i % 3 == 0:
-        num_a = num_a + 1
+        num_a = num_a + 2
     elif i % 2 == 1:
         num_b = num_b + 1
     else:
         print(i)
 print(num_a, num_b)
+print("Not a conflict")
 
 
 
@@ -93,3 +99,4 @@ blah = myString.lower()
 print(blah)
 print(myString[:5])
 print(myString[5:])
+print("I think this will conflict")
